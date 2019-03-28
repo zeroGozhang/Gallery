8ed289be4e97046a17fd71ba0946926db13d49b9
zhangwufeng <zhangwufeng@jianke.com>
2019-03-28 21:42:02 +0800

    首页

--- gallery-ui/app.js
+++ gallery-ui/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +12,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.engine('.html' , ejs.__express);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- gallery-ui/routes/index.js
+++ gallery-ui/routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Mock数据
+  const mockData = {
+    galleryHtml : req.html,
+    title: 'Gallery',
+    staticFiles : 'public/images',
+    urlRoot : '/',
+    render : false,
+    cdnDomain: '/',
+    avatar: '/images/avatar.png',
+    wording: {
+      noAccess: '抱歉，你没有权限访问'
+    },
+    albums: {
+      "铁山尖": {
+        thumbnail: "1.jpg",
+        description: '美丽的家乡',
+        name: '家乡'
+      }, 
+      "公司摄影": {
+        description : "人情似水",
+        thumbnail: "2.jpg",
+        name: "企业"
+      },
+      "雪乡": {
+        description: "美丽的哈尔滨",
+        thumbnail: "3.jpg",
+        name: "哈尔滨",
+      }
+    }
+  }
+  res.render('index', mockData);
 });
 
 module.exports = router;
