4a104db7e8da765b66453683cde5fecf5d0415ce
zhangwufeng <zhangwufeng@52tt.com>
2019-12-04 17:04:17 +0800

    修复后台逻辑

--- gallery-api/app/controller/manager/upload.js
+++ gallery-api/app/controller/manager/upload.js
@@ -12,7 +12,7 @@ class UploadController extends Controller {
   async index() {
     // egg-multipart 已经帮我们处理文件二进制对象
     // node.js操作文件流
-    const stream = await ctx.getFileStream();
+    const stream = await this.ctx.getFileStream();
     const url = await this.ctx.service.upload.index(stream);
     // 文件响应
     this.ctx.body = {
